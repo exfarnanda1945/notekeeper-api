7502ede004e509221cd1dd1201af9b7ba8371262
Exfar Nanda Micola <exfarnanda1945@gmail.com>
2022-09-19 14:26:59 +0700

    fix: implementing dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,24 @@ import fastify from "fastify";
 import { Db } from "./config";
 import { noteRoute } from "./routes";
 import consola from "consola";
+require("dotenv").config();
 
-const PORT = 5050;
-const HOST = "172.16.8.85";
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 
 function run() {
   Db.connect();
 
-  const server = fastify({ logger: true });
+  const localPort = PORT ? parseInt(PORT) : 8080;
 
+  const server = fastify({ logger: true });
   server.register(noteRoute);
 
   const start = async () => {
     try {
-      await server.listen({ port: PORT, host: HOST });
+      await server.listen({ port: localPort, host: HOST });
       consola.success(
-        `Server started at http://localhost:${PORT} or http://${HOST}:${PORT}`
+        `Server started at http://localhost:${localPort} or http://${HOST}:${localPort}`
       );
     } catch (err) {
       server.log.error(err);
